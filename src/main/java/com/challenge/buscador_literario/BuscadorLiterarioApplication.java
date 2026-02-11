dfab1a31e865ce0540c6d0b9761365a69f8b70ed
Amanda <89360779+amandaconstante@users.noreply.github.com>
2026-02-11 16:29:22 -0300

    build: initialize Spring Boot project

---
+++ src/main/java/com/challenge/buscador_literario/BuscadorLiterarioApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.buscador_literario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuscadorLiterarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuscadorLiterarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/buscador_literario/BuscadorLiterarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.buscador_literario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuscadorLiterarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
