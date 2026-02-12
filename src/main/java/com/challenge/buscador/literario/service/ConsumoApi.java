c1117defc11f3ebae89fc0e004e1242772a7cdf5
Amanda <89360779+amandaconstante@users.noreply.github.com>
2026-02-12 16:05:43 -0300

    feat: setup API consumption and JSON conversion infrastructure

---
+++ src/main/java/com/challenge/buscador/literario/service/ConsumoApi.java
@@ -0,0 +1,27 @@
+package com.challenge.buscador.literario.service;
+
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Component
+public class ConsumoApi {
+    private final HttpClient client = HttpClient.newHttpClient();
+
+    public String getResponse(String address) {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(address))
+                .build();
+        try {
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            return response.body();
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
